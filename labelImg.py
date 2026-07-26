95223b361300c2375dd49c9560b7ad5c65cc171f
Matt <ohohmygod@foxmail.com>
2026-07-26 23:59:53 +0800

    sync

--- labelImg.py
+++ labelImg.py
@@ -11,20 +11,20 @@ import webbrowser as wb
 from functools import partial
 import log
 
-try:
-    from PyQt5.QtGui import *
-    from PyQt5.QtCore import *
-    from PyQt5.QtWidgets import *
-except ImportError:
-    # needed for py3+qt4
-    # Ref:
-    # http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
-    # http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
-    if sys.version_info.major >= 3:
-        import sip
-        sip.setapi('QVariant', 2)
-    from PyQt4.QtGui import *
-    from PyQt4.QtCore import *
+# try:
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+# except ImportError:
+#     # needed for py3+qt4
+#     # Ref:
+#     # http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
+#     # http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
+#     if sys.version_info.major >= 3:
+#         import sip
+#         sip.setapi('QVariant', 2)
+#     from PyQt4.QtGui import *
+#     from PyQt4.QtCore import *
 
 from libs.combobox import ComboBox
 from libs.default_label_combobox import DefaultLabelComboBox
